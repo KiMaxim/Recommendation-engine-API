950f9c7ec2da41c22d56895b5ce707bd16aaf6a9
kimaxim <kimaxim.2005@gmail.com>
2026-03-22 20:25:05 +0800

    feat: implemented functions for authentication processes

---
+++ app/core/security.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from app.core.config import get_settings
+from typing import Optional
+
+settings = get_settings()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+def hash_password(password: str) -> str:
+    hashed_password = pwd_context.hash(password)
+    return hashed_password
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    verdict = pwd_context.verify(plain_password, hashed_password)
+    return verdict
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES) \
+        if expires_delta is None else datetime.now(timezone.utc) + expires_delta
+    to_encode.update({'exp': expire})
+    jwt_encode = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return jwt_encode
+
+def decode_access_token(token: str) -> dict:
+    try: 
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        return payload 
+    except JWTError:
+        raise JWTError("Invalid token")
+    
