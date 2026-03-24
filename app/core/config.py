48cce15dada35d615912b38bd75a09734f29ee69
kimaxim <kimaxim.2005@gmail.com>
2026-03-24 19:15:37 +0800

    feat: add new environment variables to settings

--- app/core/config.py
+++ app/core/config.py
@@ -2,20 +2,32 @@ from pydantic import BaseSettings
 from functools import lru_cache
 
 class Settings(BaseSettings):
+
+    #Application
     APP_NAME: str
     APP_VERSION: str
     DEBUG: bool 
 
-    DATABASE_URL: str 
- 
+    #Databse
+    DATABASE_URL: str
+    POOL_SIZE: int
+    MAX_OVERFLOW: int
+
+    #Security
     SECRET_KEY: str
-    ALGORITHM: str 
+    ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
+    #AI
     AI_API_KEY: str
-    EMBEDDING_MODEL: str 
+    EMBEDDING_MODEL: str
+    MAX_TOKENS: int
+    SYSTEM_INSTRUCTION: str
+
+    #Redis/Cache
     REDIS_HOST: str
-    CACHE_TTL_SECONDS: int 
+    CACHE_TTL_SECONDS: int
+
 
     class Config:
         env_file = ".env"
